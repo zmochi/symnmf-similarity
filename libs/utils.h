57b4d9a5a5ace2005a6d2a47aef1766a02eccca8
zmochi <orian755@gmail.com>
2024-09-20 11:01:14 +0300

    Added and improved utility functions

--- libs/utils.h
+++ libs/utils.h
@@ -3,12 +3,29 @@
 #ifndef __UTILS_H
 #define __UTILS_H
 
-#define LOG_ERR(fmt, ...) fprintf(stderr, "ERROR: " fmt "\n", ##__VA_ARGS__)
+#define LOG_ERR(err_msg) fprintf(stderr, "ERROR: %s" err_msg "\n", __func__)
 
-#define LOG_ABORT(fmt, ...)                                                    \
+#define LOG_ABORT(fmt)                                                         \
     do {                                                                       \
-        LOG_ERR(fmt, ##__VA_ARGS__);                                           \
+        LOG_ERR(fmt);                                                          \
         exit(1);                                                               \
     } while ( 0 )
+
+#define RETURN_ERR(fmt, ret)                                                   \
+    do {                                                                       \
+        LOG_ERR(fmt);                                                          \
+        return ret;                                                            \
+    } while ( 0 )
+
+/* assertions are used for logical program constraints and not runtime errors */
+
+#define ASSERT_MATRIX_DIM(matrix_ptr, rows, cols)                              \
+    do {                                                                       \
+        if ( matrix_ptr->num_rows != rows || matrix_ptr->num_cols != cols )    \
+            LOG_ABORT("Matrix has wrong dimensions");                          \
+    } while ( 0 )
+
+#define ASSERT_SQUARE_MATRIX(matrix_ptr)                                       \
+    ASSERT_MATRIX_DIM(matrix_ptr, matrix_ptr->num_rows, matrix_ptr->num_rows)
 
 #endif /* __UTILS_H */
