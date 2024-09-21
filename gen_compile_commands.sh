ecb5873e1f22f363fb11808881e894e054fa2b0e
zmochi <orian755@gmail.com>
2024-09-21 13:39:28 +0300

    Upgraded gen_compile_commands.sh to include all C files

--- gen_compile_commands.sh
+++ gen_compile_commands.sh
@@ -1,29 +1,51 @@
 #!/usr/bin/env bash
 
+# so C_FILES globbing includes files in project root
+shopt -s globstar
+
+# should output with -I for each include
 PYTHON_INCLUDES="$(python3-config --includes)"
 PROJ_DIR="$(pwd)"
+COURSE_COMPILE_CMD="gcc -xc -ansi -Wall -Wextra -Werror -pedantic-errors"
+PYTHON_COMPILE_CMD="gcc -xc -Wall -Wextra $PYTHON_INCLUDES"
+C_FILES=(**/*.{c,h})
+COURSE_COMPILED_FILES=()
+PYTHON_COMPILED_FILES=()
+COMPILE_COMMANDS_FILENAME=compile_commands.json
 
-cat <<EOF >compile_commands.json
-[
+echo "[" >$COMPILE_COMMANDS_FILENAME
+
+# separate project C files into module files and regular C files, filtering by 'module' in filename
+for file in "${C_FILES[@]}"; do
+	if [[ "$file" =~ .*module.* ]]; then
+		PYTHON_COMPILED_FILES+=("$file")
+	else
+		COURSE_COMPILED_FILES+=("$file")
+	fi
+done
+
+# populate compilation database
+for file in "${PYTHON_COMPILED_FILES[@]}"; do
+	cat <<EOF >>$COMPILE_COMMANDS_FILENAME
 	{
-		"command": "gcc -xc -ansi -Wall -Wextra -Werror -pedantic-errors",
+		"command": "$PYTHON_COMPILE_CMD",
 		"directory": "$PROJ_DIR",
-		"file": "symnmf.c"
+		"file": "$file"
 	},
-	{
-		"command": "gcc -xc -ansi -Wall -Wextra -Werror -pedantic-errors",
-		"directory": "$PROJ_DIR",
-		"file": "symnmf.h"
-	},
-	{
-		"command": "gcc -xc -Wall -Wextra $PYTHON_INCLUDES",
-		"directory": "$PROJ_DIR",
-		"file": "symnmfmodule.c"
-	},
-	{
-		"command": "gcc -xc -Wall -Wextra $PYTHON_INCLUDES",
-		"directory": "$PROJ_DIR",
-		"file": "symnmfmodule.h"
-	}
-]
 EOF
+done
+
+for file in "${COURSE_COMPILED_FILES[@]}"; do
+	cat <<EOF >>$COMPILE_COMMANDS_FILENAME
+	{
+		"command": "$COURSE_COMPILE_CMD",
+		"directory": "$PROJ_DIR",
+		"file": "$file"
+	},
+EOF
+done
+
+# remove trailing , and newline from last entry
+truncate -s -2 $COMPILE_COMMANDS_FILENAME
+
+echo -ne "\n]" >>$COMPILE_COMMANDS_FILENAME
