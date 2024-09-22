3bcb6b07ec64ea76dd79331cd38824f76a2862e8
Hila Ofek <hilaofek1@mail.tau.ac.il>
2024-09-22 10:07:33 +0300

    added transpose_matrices

--- libs/matrix.c
+++ libs/matrix.c
@@ -50,6 +50,7 @@ int main(void) { /*for checks, delete later*/
             set_matrix_elem(m2, i, j, i + j);
         }
     }
+    /*
     for ( i = 0; i < m2->num_rows; i++ ) {
         for ( j = 0; j < m2->num_cols; j++ ) {
             printf("%f ", m2->data[i][j]);
@@ -63,7 +64,23 @@ int main(void) { /*for checks, delete later*/
         }
         printf("\n");
     }
+    */
+    result = get_empty_matrix(m1->num_cols, m1->num_rows);
+    transpose_matrix(m1, result);
+    for ( i = 0; i < m1->num_rows; i++ ) {
+        for ( j = 0; j < m1->num_cols; j++ ) {
+            printf("%f ", m1->data[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 
+    for ( i = 0; i < result->num_rows; i++ ) {
+        for ( j = 0; j < result->num_cols; j++ ) {
+            printf("%f ", result->data[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
 
@@ -102,7 +119,7 @@ struct matrix *get_empty_matrix(m_index rows, m_index cols) {
     return new_mat;
 }
 
-int free_matrix(struct matrix *matrix) {
+int free_matrix(matrix *matrix) {
     m_index i;
     for ( i = 0; i < matrix->num_rows; i++ ) {
         free(matrix->data[i]);
@@ -112,18 +129,17 @@ int free_matrix(struct matrix *matrix) {
     return 0;
 }
 
-matrix_element set_matrix_elem(struct matrix *matrix, m_index i, m_index j,
+matrix_element set_matrix_elem(matrix *matrix, m_index i, m_index j,
                                matrix_element elem) {
     matrix->data[i][j] = elem;
     return matrix->data[i][j];
 }
 
-matrix_element get_matrix_elem(const struct matrix *matrix, m_index i,
-                               m_index j) {
+matrix_element get_matrix_elem(const matrix *matrix, m_index i, m_index j) {
     return matrix->data[i][j];
 }
 
-matrix_element *get_matrix_vec(const struct matrix *matrix, m_index i) {
+matrix_element *get_matrix_vec(const matrix *matrix, m_index i) {
     return matrix->data[i];
 }
 
@@ -133,7 +149,7 @@ int set_matrix_vec(struct matrix *matrix, m_index i, matrix_element *vec) {
     return 0;
 }
 
-int multiply_matrices(const struct matrix *m1, const struct matrix *m2,
+int multiply_matrices(const matrix *m1, const matrix *m2,
                       struct matrix *result) {
     m_index i, j, k;
     ASSERT_MATRIX_DIM(m1, m1->num_rows, m2->num_rows);
@@ -150,7 +166,7 @@ int multiply_matrices(const struct matrix *m1, const struct matrix *m2,
 
     return 0;
 }
-int subtract_matrices(const struct matrix *m1, const struct matrix *m2,
+int subtract_matrices(const matrix *m1, const matrix *m2,
                       struct matrix *result) {
     m_index i, j;
     ASSERT_MATRIX_DIM(m1, m2->num_rows, m2->num_cols);
@@ -161,5 +177,14 @@ int subtract_matrices(const struct matrix *m1, const struct matrix *m2,
         }
     }
 
+    return 0;
+}
+
+int transpose_matrix(const matrix *matrix, struct matrix *transposed) {
+    m_index i, j;
+    ASSERT_MATRIX_DIM(transposed, matrix->num_cols, matrix->num_rows);
+    for ( i = 0; i < transposed->num_rows; i++ )
+        for ( j = 0; j < transposed->num_cols; j++ )
+            transposed->data[i][j] = matrix->data[j][i];
     return 0;
 }
