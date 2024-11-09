af48b0a0232a84a5a6162b4216770c0ac7be464f
Hila Ofek <hilaofek1@mail.tau.ac.il>
2024-11-09 12:09:13 +0200

    added comments

--- analysis.py
+++ analysis.py
@@ -18,18 +18,19 @@ def main():
     score_for_nmf(points,k)
     score_for_kmeans(points,k)
    
-
+# calculate score from kmeans clustering from ex1
 def score_for_kmeans(points, k):
     kms_lables=kMeans(points,k)
     score = silhouette_score(points, kms_lables, metric='euclidean')
     print(f"kmeans: {score:.4f}")
-
+# calculate score from symnmf clustering
 def score_for_nmf(points,k):
     final_H=parse_goal_and_calc(k,"symnmf",points)
     labels=cluster_association(final_H)
     score = silhouette_score(points, labels, metric='euclidean')
     print(f"nmf: {score:.4f}")
 
+# determine clusters according to provided algorithm
 def cluster_association(final_H):
     labels=[0 for i in range(len(final_H))]
     for i in range(len(final_H)):
