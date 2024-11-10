1a5fd009ca18ed47020eb249f0d04247dd2a3c6f
Hila Ofek <hilaofek1@mail.tau.ac.il>
2024-11-10 09:07:55 +0200

    imported functions from symnmf.py instead of repeating code

--- analysis.py
+++ analysis.py
@@ -6,6 +6,9 @@ from kmeans import kMeans
 import math
 import symnmfmodule
 from symnmf import parse_goal_and_calc
+from symnmf import parse_cmdline_arg
+from symnmf import parse_points
+from symnmf import parse_k
 max_iter = 300
 epsilon=0.0001
 beta = 0.5
@@ -23,6 +26,7 @@ def score_for_kmeans(points, k):
     kms_lables=kMeans(points,k)
     score = silhouette_score(points, kms_lables, metric='euclidean')
     print(f"kmeans: {score:.4f}")
+    
 # calculate score from symnmf clustering
 def score_for_nmf(points,k):
     final_H=parse_goal_and_calc(k,"symnmf",points)
@@ -43,39 +47,6 @@ def cluster_association(final_H):
         labels[i]=cluster
     return labels
 
-
-def parse_cmdline_arg(arg_str):
-    try:
-        parsed_arg = float(arg_str)
-    except ValueError:
-        return -1  # indicate error with -1 since commandline args shouldn't be negative
-
-    if parsed_arg - int(parsed_arg) != 0:
-        return -1  # indicate error with -1 since commandline args shouldn't be negative
-    return int(parsed_arg)
-
-def parse_points(text_name):
-    points=[]
-    type = text_name[-4:]
-    if not (type == ".txt"):
-        print("An Error Has Occurred")
-        exit(1)
-    f = open(text_name, "r")
-    for x in f:
-        x = x.replace("\n", "")
-        pointstr = x.split(",")
-        point = []
-        for str in pointstr:
-            point.append(float(str))
-        points.append(point)
-    return points
-
-def parse_k(k, len_points):
-    k = parse_cmdline_arg(k)
-    if not (k > 1 and k < len_points):
-        print("Invalid number of clusters!")
-        exit(1)
-    return k
 
 
 main()
